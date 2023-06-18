a6d23fcc08269d3ec0aecbde64e7908ec51669f1
Sergey Ostroukh <hardmass3ffect@gmail.com>
2023-06-18 17:37:26 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,44 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
 
-            return state // need to fix
+    switch (action.type) {
+        case 'sort': {
+            if (action.payload === "up") {
+                state.sort(function (a, b) {
+                    let nameA = a.name
+                    let nameB = b.name
+                    if (nameA < nameB) {
+                        return -1
+                    }
+                    if (nameA > nameB) {
+                        return 1
+                    }
+                    return 0
+                })
+            }
+            else if (action.payload === "down"){
+                state.sort(function (b, a) {
+                    let nameA = a.name
+                    let nameB = b.name
+                    if (nameA < nameB) {
+                        return -1
+                    }
+                    if (nameA > nameB) {
+                        return 1
+                    }
+                    return 0
+                })
+            }
+
+            return [...state]
         }
         case 'check': {
+                return state.filter(el=>el.age>=18)
 
-            return state // need to fix
+
+           // need to fix
         }
         default:
             return state
